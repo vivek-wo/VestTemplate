9a969d180feb8c333a86d83e2f87bbc61e92fc9c
vivek-wo <vivek.wei.wu@gmail.com>
2019-02-28 17:56:54 +0800

    实现启动页

---
+++ app/src/main/java/com/vest/template/GuideActivity.kt
@@ -0,0 +1,15 @@
+package com.vest.template
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class GuideActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/vest/template/SplashActivity.kt
+++ app/src/main/java/com/vest/template/SplashActivity.kt
@@ -1,12 +1,27 @@
 package com.vest.template
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
-class SplashActivity : AppCompatActivity(){
+//TODO 背景图片设置在Theme中，存在内存占用问题
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fitHideNavigation()
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, GuideActivity::class.java))
+        }, 3 * 1000)
+    }
+
+    private fun fitHideNavigation() {
+        val decorView = window.decorView
+        val options = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        decorView.systemUiVisibility = options
     }
 
     override fun onDestroy() {
