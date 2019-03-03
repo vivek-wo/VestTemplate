2f914f3248fe3856d74fcb9637db1034deec11b3
vivek-wo <vivek.wei.wu@gmail.com>
2019-03-04 01:02:39 +0800

    实现Zodiac数据获取

---
+++ app/src/main/java/com/vest/template/sample/zodiac/ZodiacData.kt
@@ -0,0 +1,8 @@
+package com.vest.template.sample.zodiac
+
+data class ZodiacData(
+    var index: Int,
+    var iconName: String,
+    var name: String,
+    var description: String
+)
--- app/src/main/java/com/vest/template/sample/zodiac/ZodiacFragment.kt
+++ app/src/main/java/com/vest/template/sample/zodiac/ZodiacFragment.kt
@@ -2,6 +2,7 @@ package com.vest.template.sample.zodiac
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,8 @@ import com.vest.template.sample.MainPresenter
 import com.vest.template.sample.R
 
 class ZodiacFragment : Fragment(), ZodiacView {
+    lateinit var zodiacPresenter: ZodiacPresenter
+
     override fun setPresenter(presenter: MainPresenter?) {
     }
 
@@ -19,5 +22,30 @@ class ZodiacFragment : Fragment(), ZodiacView {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        zodiacPresenter = ZodiacPresenter(context!!)
+        zodiacPresenter.takeView(this)
+        zodiacPresenter.getData()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        zodiacPresenter.dropView()
+    }
+
+    class GridRecyclerAdapter(val zodiacDataList: List<ZodiacData>) :
+        RecyclerView.Adapter<GridRecyclerAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+            val v = LayoutInflater.from(p0!!.context).inflate(R.layout.item_zodiac_grid, p0, false)
+            return ViewHolder(v)
+        }
+
+        override fun getItemCount(): Int = zodiacDataList.size
+
+        override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        }
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        }
     }
 }
